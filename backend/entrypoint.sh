807475abe740e23f0dab0c65cae7019a732ad920
Kariuki Antony <antonygichoya9@gmail.com>
2024-04-24 22:07:41 +0300

    add the entrypoint to the backend app

---
+++ backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [ "$DATABASE" == "postgres" ]; then
+    echo "postgres starting in a few ..."
+
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+        echo "waiting for a tcp connection ..."
+        sleep 0.1
+    done
+
+    echo "postgresql started"
+
+fi
+
+exec "$@"
